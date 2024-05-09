48675d8e71163af174c034c5c832f93ea1a97508
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-05-09 22:58:33 +0530

    Code1.py

---
+++ Unit 1/Code1.py
@@ -0,0 +1,24 @@
+x= 12
+y=14
+print("Before swapping:",x,y)
+x,y=y,x
+print("After swapping:",x,y)
+
+p,q,r= 5,10,7
+q,r,p= p+1,q+2,r-1
+print(p,q,r)
+
+a="hello"
+print(type(x))    # Using 'type' function
+print(type(a))
+
+print(id(x))      # Using 'id' function
+print(id(55))
+
+A=12
+B=14
+C=16
+print(A,B,C,end='')
+print('\t')             
+print(A,B,C,sep='@',end='')
+
