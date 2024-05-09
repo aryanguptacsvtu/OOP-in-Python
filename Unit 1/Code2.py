3426796c0c579b9c66753290862ab46bbd780108
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-05-09 23:09:27 +0530

    Code2.py

---
+++ Unit 1/Code2.py
@@ -0,0 +1,21 @@
+# PRIMTIVE DATATYPES:-
+w=1          
+z=2.32
+x=1j
+a=4+3j
+c=5e4
+str="hello  world!"
+print(c)
+print(a.imag)
+print(bool(34))
+print(bool(-12))
+print(bool(''))
+print(bool(0))
+
+
+#COLLECTION DATATYPES:-
+
+list=[12 ,45, 54.5, 'HII']
+tuple=(11, 33, 44, 'nice', 6.4)
+set={32, 43, 6,'Csvtu', 6.66}
+dictionary={1:43, 2:54, "cg":'raipur'}
