e91309f27a942b2035bdc2f2ce045a117962e54f
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-05-10 11:06:45 +0530

    Code3.py

---
+++ Unit 1/Code3.py
@@ -0,0 +1,59 @@
+# ARITHMETIC OPERATORS:-
+a=14  
+b=4
+print(a+b) , print(a-b)
+print(a*b) 
+print(a/b)     # Division(float)
+print(a//b)    # Division(floor)
+print(a%b)
+print(a**b)
+print("\t")
+
+
+#RELATONAL OPERATORS:-
+x=100
+y=43
+print(x>y)  , print(x>=y)
+print(x<y) , print(x<=y)
+print(x==y) , print(x!=y)
+print("\t")
+
+
+# LOGICAL OPERATORS:-
+print(5>8 and 7>3)
+print(5>8 or 7>3)
+
+
+# BITWISE OPERATORS:-
+print(8 & 1)
+print(8 | 1)
+print(8 ^ 1)
+print(~8)
+print(8<<2) , print(8>>2)
+print("\t")
+
+
+# ASSIGNMENT OPERATORS:-
+D=100
+E=20
+D+=E 
+print(D)
+D *=E
+print(D)
+D &=E
+print(D)
+print("\t")
+
+
+# SPECIAL OPERATORS:-
+a1=3
+b1=3
+print(a1 is b1)        # Identity Operators
+x1=[1,2,3]
+y1=[1,2,3]
+print(x1 is y1)
+print(x1 is not y1)
+
+x="Digital India"
+print('D' in x)        # Membership Operators
+print('X' not in x)
