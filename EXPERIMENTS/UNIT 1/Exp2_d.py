3a99a696e6a03d08996fd4caed6773d1c4e6829d
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-05-12 10:51:31 +0530

    Exp2_d.py

---
+++ EXPERIMENTS/UNIT 1/Exp2_d.py
@@ -0,0 +1,13 @@
+dict = {'name':'Aryan','age':19,'eligible':True}
+print("Length of Dictionary:",len(dict))
+print('\t')
+
+print("Indexing Operations:")
+print(dict['age'])
+print(dict['name'])
+print(dict.values())
+print('\t')
+
+s={1,34,54,'world',65}
+print("Length of Set:",len(s))
+print('\t')
