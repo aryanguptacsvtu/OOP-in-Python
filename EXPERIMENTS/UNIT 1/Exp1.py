e86a8e562f891e21ffe4e314260e867b1cb653cc
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-05-12 10:47:29 +0530

    Exp1.py

---
+++ EXPERIMENTS/UNIT 1/Exp1.py
@@ -0,0 +1,9 @@
+a= int(input("Enter an integer:"))
+b=float(input("Enter a float value:"))
+c=input("Enter the name:")
+d=complex(input("Enter a complex no.:"))
+
+print("Your first number is:",a)
+print("Your second number is:",b)
+print("Your name is:",c)
+print("Your complex number is:",d)
