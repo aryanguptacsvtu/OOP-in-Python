24ffcedb17fdcde93ca02ee53cbd00d58b8ed223
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-05-14 12:49:11 +0530

    OperatorOverloading.py

---
+++ OperatorOverloading.py
@@ -0,0 +1,21 @@
+class Vector:
+    def __init__(self,i,j,k):
+        self.i = i
+        self.j = j
+        self.k = k
+
+    def __str__(self):
+        return f'{self.i}i + {self.j}j + {self.k}k'
+    
+    def __add__(self,x):
+        return Vector(self.i+x.i , self.j+x.j , self.k+x.k)
+
+    
+v1 = Vector(1,3,4)
+print(v1)
+
+v2 = Vector(6,8,3)
+print(v2)
+
+print(v1+v2)
+print(type(v1+v2))
