10ca2cb152c9480b2d121b8c423591543dd45535
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-06-02 12:23:53 +0530

    Exp1

---
+++ EXPERIMENTS/UNIT 2/Exp1(package)/funcall.py
@@ -0,0 +1,5 @@
+import module1 as m
+
+print("Multiplication:",m.multi(10,2))
+print("Subtraction:",m.sub(8,2))
+print("Addition:",m.add(10,14))
---
+++ EXPERIMENTS/UNIT 2/Exp1(package)/module1.py
@@ -0,0 +1,8 @@
+def multi(a,b):
+    return a*b
+
+def sub(a,b):
+    return a-b
+
+def add(a,b):
+    return a+b
