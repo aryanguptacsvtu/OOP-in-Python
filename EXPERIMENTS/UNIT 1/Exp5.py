17a525005873b774c4cb6ba66b15f4c03134597c
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-06-02 12:17:57 +0530

    Exp5.py

---
+++ EXPERIMENTS/UNIT 1/Exp5.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+list = [1,34,45,56,3,76]
+print(random.choice(list))
+print(random.randrange(20,50,3))
+print('\t')
+
+print(math.ceil(3.14))
+print(math.factorial(5))
+print(math.pow(10,3))
+print(math.log(100 ,10))
+print(math.exp(2))
